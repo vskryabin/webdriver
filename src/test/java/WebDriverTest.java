fc6ba28896b12777d776dcd6102ced990b08c79c
vskryabin <vs8199@gmail.com>
2025-05-15 18:03:36 -0700

    Proj sample

---
+++ src/test/java/WebDriverTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void before() {
+        System.out.println("beforeEach");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("afterEach");
+        driver.quit();
+    }
+
+    @DisplayName("Quote Test")
+    @Test
+    public void quoteTest() {
+        System.out.println("quoteTest");
+        driver.get("https://skryabin.com/market/quote.html");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("jdoe");
+    }
+
+    @DisplayName("Another Test")
+    @Test
+    public void anotherTest() {
+        System.out.println("anotherTest");
+        driver.get("https://skryabin.com/market/quote.html");
+        driver.findElement(By.id("formSubmit")).click();
+    }
+
+
+}
